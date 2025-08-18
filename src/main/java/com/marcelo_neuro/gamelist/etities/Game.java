875f460bdadb7a9ade6b7bad6c2d650f8aa965c4
marcelo-neuro <marcelofurla0813@gmail.com>
2025-08-18 15:26:18 -0300

    Created entity games; Lombok added

---
+++ src/main/java/com/marcelo_neuro/gamelist/etities/Game.java
@@ -0,0 +1,27 @@
+package com.marcelo_neuro.gamelist.etities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String plataforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
