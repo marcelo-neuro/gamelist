4f3965b04c95a4a347232e5c6fec505fb4e6556e
marcelo-neuro <marcelofurla0813@gmail.com>
2025-08-19 21:32:58 -0300

    Created findById endpoint

--- src/main/java/com/marcelo_neuro/gamelist/controllers/GameController.java
+++ src/main/java/com/marcelo_neuro/gamelist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.marcelo_neuro.gamelist.controllers;
 
+import com.marcelo_neuro.gamelist.dto.GameDTO;
 import com.marcelo_neuro.gamelist.dto.GameMinDTO;
 import com.marcelo_neuro.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,12 @@ public class GameController {
     @GetMapping
     public ResponseEntity<List<GameMinDTO>> findAll() {
         List<GameMinDTO> result = service.findAll();
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GameDTO> findById(@PathVariable Long id) {
+        GameDTO result = service.findById(id);
         return ResponseEntity.ok(result);
     }
 }
--- src/main/java/com/marcelo_neuro/gamelist/services/GameService.java
+++ src/main/java/com/marcelo_neuro/gamelist/services/GameService.java
@@ -1,12 +1,16 @@
 package com.marcelo_neuro.gamelist.services;
 
+import com.marcelo_neuro.gamelist.dto.GameDTO;
 import com.marcelo_neuro.gamelist.dto.GameMinDTO;
+import com.marcelo_neuro.gamelist.etities.Game;
 import com.marcelo_neuro.gamelist.repositories.GameRepository;
+import com.marcelo_neuro.gamelist.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -14,10 +18,20 @@ public class GameService {
     @Autowired
     private GameRepository repository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return repository.findAll()
                 .stream().map(GameMinDTO::new)
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Optional<Game> optionalGame = repository.findById(id);
+        if(optionalGame.isEmpty()) {
+            throw new ResourceNotFoundException("Recurso não encontrado ; id: " + id);
+        }
+
+        return new GameDTO(optionalGame.get());
     }
 }
