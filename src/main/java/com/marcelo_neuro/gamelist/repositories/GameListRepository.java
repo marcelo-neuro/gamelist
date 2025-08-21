05020de46d92b0bca5a84eec0dbccb368dbdd56e
Marcelo Henrique Barbosa Furlanetto <44544494818@intra.pgfn.gov.br>
2025-08-21 16:05:47 -0300

    Endpoint especial incompleto

--- src/main/java/com/marcelo_neuro/gamelist/repositories/GameListRepository.java
+++ src/main/java/com/marcelo_neuro/gamelist/repositories/GameListRepository.java
@@ -1,10 +1,22 @@
 package com.marcelo_neuro.gamelist.repositories;
 
+import com.marcelo_neuro.gamelist.etities.Belonging;
 import com.marcelo_neuro.gamelist.etities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GameListRepository extends JpaRepository<GameList, Long> {
 
+
+    @Query(value = """
+            SELECT b
+            FROM Belonging b
+            WHERE b.id.list.id = :listId
+            ORDER BY b.position
+            """)
+    List<Belonging> querry(Long listId);
 }
--- src/main/java/com/marcelo_neuro/gamelist/services/GameListService.java
+++ src/main/java/com/marcelo_neuro/gamelist/services/GameListService.java
@@ -1,6 +1,8 @@
 package com.marcelo_neuro.gamelist.services;
 
 import com.marcelo_neuro.gamelist.dto.GameListDTO;
+import com.marcelo_neuro.gamelist.etities.Belonging;
+import com.marcelo_neuro.gamelist.etities.Game;
 import com.marcelo_neuro.gamelist.repositories.GameListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +21,31 @@ public class GameListService {
         return repository.findAll()
                 .stream().map(GameListDTO::new)
                 .toList();
+    }
+
+    // incompleto
+    @Transactional
+    public List<Belonging> updatePosition(Long listId, Long gameID, Integer finalPosition) {
+        List<Belonging> belongings = repository.querry(listId);
+
+        Belonging game = belongings
+                .stream().filter(b -> b.getId().getGame().getId().equals(gameID))
+                .toList().getFirst();
+
+        for (int i = 0; i < belongings.size() ; i++) {
+
+            if(i < game.getPosition()) {
+                continue;
+            }
+
+            if(i > finalPosition + 1) {
+                break;
+            }
+
+            belongings.get(i).setPosition(belongings.get(i).getPosition() + 1);
+
+
+        }
+        return belongings;
     }
 }
